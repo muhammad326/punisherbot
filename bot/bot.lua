5f2c03743ce02025170f4e576c8e477b17e151da
muhammad326 <muhammad326@users.noreply.github.com>
2016-08-09 18:07:42 +0430

    Update

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {@frank15003},
+    sudo_users = {111105751},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
